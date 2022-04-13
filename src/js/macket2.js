b8472bbc0613ea74f3a3230fb7f7d05d8af4793f
konstraint <cheykinkostya@gmail.git config --global user.name>
2022-04-13 13:15:03 +0300

    macket2_desktop

--- src/js/macket2.js
+++ src/js/macket2.js
@@ -1,1 +1,11 @@
 import * as bootstrap from 'bootstrap';
+
+$('.mycustom-scroll').mCustomScrollbar({
+    axis: 'y',              // вертикальный скролл 
+    //theme: 'rounded-dark',  // тема 
+    scrollInertia: '330',   // продолжительность прокрутки, значение в миллисекундах 
+    setHeight: '163px',      // высота блока (переписывает CSS) 
+    mouseWheel: {
+        deltaFactor: 300    // кол-во пикселей на одну прокрутку колёсика мыши 
+    }
+});
